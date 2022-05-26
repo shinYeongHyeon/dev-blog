f925bb826ed09c3da3e29545ff329d2e58d9a189
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-05-26 21:40:21 +0900

    FEATURE: 포스트 목록 내 시리즈

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -6,11 +6,11 @@ import map from 'lodash/map';
 import filter from 'lodash/filter';
 import findIndex from 'lodash/findIndex';
 import flattenDeep from 'lodash/flattenDeep';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
-import { Typography } from '@mui/material';
 
 import DefaultLayout from 'layout/DefaultLayout';
 import FullLayout from 'layout/FullLayout';
@@ -18,15 +18,22 @@ import DefaultHead from 'component/DefaultHead';
 import PostList from 'component/PostList';
 import HashtagList from 'component/HashtagList';
 import { allPosts } from 'posts/AllPosts';
+import SeriesList from '../../component/SeriesList';
 
-const usePosts = (searchTag?: string | undefined) => {
+const usePosts = (
+    searchTag?: string | undefined,
+    searchSeries?: string | undefined,
+) => {
     const posts = searchTag
         ? filter(
               allPosts,
               ({ tags }) => findIndex(tags, (tag) => tag === searchTag) !== -1,
           )
+        : searchSeries
+        ? filter(allPosts, ({ seriesId }) => seriesId === searchSeries)
         : allPosts;
     const hashtags = uniq(flattenDeep(map(allPosts, ({ tags }) => tags)));
+    const series = uniq(map(allPosts, ({ seriesId }) => seriesId));
 
     const postComponents = map(
         posts,
@@ -48,19 +55,25 @@ const usePosts = (searchTag?: string | undefined) => {
             <HashtagList key={tag} tag={tag} />
         </Box>
     ));
+    const seriesComponents = map(series, (aSeries: string) => (
+        <Box mx={1} key={`box-${aSeries}`}>
+            <SeriesList key={aSeries} series={aSeries} />
+        </Box>
+    ));
 
     return {
         models: {
             postComponents,
             hashtagComponents,
+            seriesComponents,
         },
     };
 };
 
 const Posts: NextPage = () => {
     const router = useRouter();
-    const { tag } = router.query;
-    const { models } = usePosts(tag as string);
+    const { tag, series } = router.query;
+    const { models } = usePosts(tag as string, series as string);
 
     return (
         <FullLayout>
@@ -71,15 +84,27 @@ const Posts: NextPage = () => {
             <Box py={5}>
                 <Divider />
             </Box>
-            <Grid container>
+            <Grid container spacing={1}>
                 <Grid item xs={2}>
+                    <Box display={'flex'} justifyContent={'center'}>
+                        <Typography variant={'h6'}>Tags</Typography>
+                    </Box>
                     <Link href={`/posts/`}>
                         <Typography variant={'h6'}>전체 보기</Typography>
                     </Link>
                     {models.hashtagComponents}
                 </Grid>
                 <Grid item xs={8}>
-                    {models.postComponents}
+                    <Box>{models.postComponents}</Box>
+                </Grid>
+                <Grid item xs={2}>
+                    <Box display={'flex'} justifyContent={'center'}>
+                        <Typography variant={'h6'}>Series</Typography>
+                    </Box>
+                    <Link href={`/posts/`}>
+                        <Typography variant={'h6'}>전체 보기</Typography>
+                    </Link>
+                    {models.seriesComponents}
                 </Grid>
             </Grid>
         </FullLayout>
